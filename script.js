dc9e4ff998b4f5df1a0581b7afffc06cf43f24f1
Daniel Jacobson <dtjacobson03@gmail.com>
2023-03-21 17:27:51 -0700

    add all outcomes of rock paper scissors in a function and change phrase to winner or loser

--- script.js
+++ script.js
@@ -9,3 +9,29 @@ const getComputerChoice = () => {
 };
 
 console.log(getComputerChoice());
+
+const playRound = (playerSelection, computerSelection) => {
+    let conclusion = 'nothing selected';
+    if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        conclusion = 'You Win! Rock beats scissors';
+    } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+            conclusion = 'You Lose! Scissors beats Paper'
+    } else if (playerSelection === 'scissors' && computerSelection === 'scissors') {
+            conclusion = 'Tie!'
+    } else if (playerSelection === 'rock' && computerSelection === 'paper') {
+        conclusion = 'You lose! Paper beats Rock'
+    } else if (playerSelection === 'rock' && computerSelection === 'rock') {
+        conclusion = 'Tie!'
+    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        conclusion = 'You Win! Paper beats rock'
+    } else if (playerSelection === 'paper' && computerSelection === 'paper') {
+        conclusion = 'Tie!'
+    } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+        conclusion = 'You Lose! Rock beats scissors'
+    } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        conclusion = 'You Win! scissors beats paper'
+    }
+    console.log(conclusion)
+}
+
+playRound();
