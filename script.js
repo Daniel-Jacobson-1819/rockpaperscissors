2e9e12e9ade8aa3220a7c95e7163d401d30979bd
Daniel Jacobson <dtjacobson03@gmail.com>
2023-03-22 14:53:12 -0700

    create functions to play game and make sure it works

--- script.js
+++ script.js
@@ -11,7 +11,42 @@ const getComputerChoice = () => {
     } else if (randomNum === 2) {
         compChoice = 'scissors'
     }
-    console.log(compChoice);
+    return compChoice
+    
+}
+getComputerChoice();
+
+// play the round and input a conclusion message based on outcome
+
+const playRound = (playerChoice, computerChoice) => {
+    let conclusion
+    
+    if (playerChoice === 'rock' &&  computerChoice === 'rock') {
+        conclusion = 'It is a Tie!'
+    } else if (playerChoice === 'rock' && computerChoice === 'paper') {
+        conclusion = 'You Lose! Paper beats Rock'
+    } else if (playerChoice === 'rock' && computerChoice === 'scissors') {
+        conclusion = 'You Win! Rock beats Scissors'
+    } else if (playerChoice === 'paper' && computerChoice === 'rock') {
+        conclusion = 'You Win! Paper beats Rock'
+    } else if (playerChoice === 'paper' && computerChoice === 'paper') {
+        conclusion = 'It is a Tie!'
+    } else if (playerChoice === 'paper' && computerChoice === 'scissors') {
+        conclusion = 'You Lose! Scissors beats Paper'
+    } else if (playerChoice === 'scissors' && computerChoice === 'rock') {
+        conclusion = 'You Lose! Rock beats scissors'
+    } else if (playerChoice === 'scissors' && computerChoice === 'paper') {
+        conclusion = 'You Win! Scissors beats paper'
+    } else if (playerChoice === 'scissors' && computerChoice === 'scissors') {
+        conclusion = 'It is a Tie!'
+    } 
+
+    return conclusion 
 }
 
-getComputerChoice();
+const playerChoice = 'rock';
+const computerChoice = getComputerChoice();
+
+console.log(playerChoice, computerChoice)
+
+console.log(playRound(playerChoice, computerChoice));
