07b64450a9c9ee0d1b4e5c51edb2b7771ad847f5
Daniel Jacobson <dtjacobson03@gmail.com>
2023-03-21 17:09:28 -0700

    add comment

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// get the computers choice of rock paper or scissor using math.random to find a random string from the array corresponding with the choice
 const getComputerChoice = () => {
     const rpsArray = ['rock', 'paper', 'scissors'];
 
