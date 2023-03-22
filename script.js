dec1c6c5281f0758432f9370b5569f2ab06c79ea
Daniel Jacobson <dtjacobson03@gmail.com>
2023-03-22 14:21:34 -0700

    restart from scratch with fresh mind because old code didnt work how i wanted

--- script.js
+++ script.js
@@ -1,41 +1,18 @@
-// get the computers choice of rock paper or scissor using math.random to find a random string from the array corresponding with the choice
+// find a random number and assign a string based on the number (rock, paper, scissors)
+
 const getComputerChoice = () => {
-    const rpsArray = ['rock', 'paper', 'scissors'];
+    let randomNum = Math.floor(Math.random() * 3);
+    let compChoice = '';
 
-    const random = Math.floor(Math.random() * rpsArray.length)
-
-    return rpsArray[random]
-
-};
-
-
-
-const playRound = (playerSelection, computerSelection) => {
-    let conclusion = 'nothing selected';
-    if (playerSelection === 'rock' && computerSelection === 'scissors') {
-        conclusion = 'You Win! Rock beats scissors';
-    } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
-            conclusion = 'You Lose! Scissors beats Paper'
-    } else if (playerSelection === 'scissors' && computerSelection === 'scissors') {
-            conclusion = 'Tie!'
-    } else if (playerSelection === 'rock' && computerSelection === 'paper') {
-        conclusion = 'You lose! Paper beats Rock'
-    } else if (playerSelection === 'rock' && computerSelection === 'rock') {
-        conclusion = 'Tie!'
-    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
-        conclusion = 'You Win! Paper beats rock'
-    } else if (playerSelection === 'paper' && computerSelection === 'paper') {
-        conclusion = 'Tie!'
-    } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
-        conclusion = 'You Lose! Rock beats scissors'
-    } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
-        conclusion = 'You Win! scissors beats paper'
+    if (randomNum === 0){
+        compChoice = 'rock'
+    } else if (randomNum === 1) {
+        compChoice = 'paper'
+    } else if (randomNum === 2) {
+        compChoice = 'scissors'
     }
-    console.log(playerSelection, computerSelection)
-    return conclusion
+
+
+    console.log(compChoice);
 }
-
-const playerSelection = 'rock';
-const computerSelection = getComputerChoice();
-
-console.log(playRound(playerSelection, computerSelection));
+getComputerChoice();
